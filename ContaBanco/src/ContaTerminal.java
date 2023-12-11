44677e4fbe42c3e1b82250535be95193cbcd58b3
Desenvolvedor Back-end Cleidsan <dsanmike12@gmail.com>
2023-12-11 20:42:56 -0300

    Testando o uso do terminal

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,16 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String numero = args [0];//teclado.nextInt();
+
+        System.out.println("Por favor, digite seu Nome");
+        String nomeCliente = args [1]; //teclado.next();
+
+        System.out.println("Por favor, digite seu Sobrenome");
+        String agencia = args [2];//"067-8";
+
+        String saldo = args [3];//0;
+
+    }
+}

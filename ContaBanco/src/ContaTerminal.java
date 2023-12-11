f75ef57a0b72804a155ebd11f6e306e34f7b0baf
Desenvolvedor Back-end Cleidsan <dsanmike12@gmail.com>
2023-12-11 20:44:08 -0300

    Estrando com a interação do teclado cliente

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,16 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
 
+        // System.out.println("Por favor, digite o número da Agência !");
+        // String numero = args [0];//teclado.nextInt();
+
+        // System.out.println("Por favor, digite seu Nome");
+        // String nomeCliente = args [1]; //teclado.next();
+
+        // System.out.println("Por favor, digite seu Sobrenome");
+        // String agencia = args [2];//"067-8";
+
+        // String saldo = args [3];//0;
+    
+        Scanner teclado = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta !");
+        int numero = teclado.nextInt();
+
         System.out.println("Por favor, digite o número da Agência !");
-        String numero = args [0];//teclado.nextInt();
+        String agencia = teclado.next();
 
         System.out.println("Por favor, digite seu Nome");
-        String nomeCliente = args [1]; //teclado.next();
+        String nomeCliente = teclado.next();
 
-        System.out.println("Por favor, digite seu Sobrenome");
-        String agencia = args [2];//"067-8";
+        System.out.println("Por favor, digite seu Saldo");
+        double saldo = teclado.nextDouble();
 
-        String saldo = args [3];//0;
-
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia + ", conta " + numero + " e seu saldo "+ saldo +" já está disponível para saque");
     }
 }
